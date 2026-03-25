ae44ff458376295bd1501626b9e8942bfe139139
Tanmay Mangesh Pawar <pawartanmay045@gmail.com>
2026-03-25 17:44:35 +0530

    fix: add Link import

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import axios from "axios"
+import { Link } from "react-router-dom"
 import { useNavigate } from "react-router-dom"
 function Login(){
 const [email,setEmail] = useState("")
